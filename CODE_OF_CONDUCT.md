9e12f9efd312a2b0472b5db7a85fbe427feb081d
k4my4r <iamkamyr@gmail.com>
2025-09-28 13:33:05 -0400

    modified day_1

--- CODE_OF_CONDUCT.md
+++
@@ -1,3 +0,0 @@
-# Code of Conduct
-
-Unacceptable behavior may be reported to: iamkamyr@gmail.com
--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from camm_hackathon.main import main
-
-def test_main(capsys):
-    main()
-    captured = capsys.readouterr()
-    assert "Hello, world!" in captured.out
